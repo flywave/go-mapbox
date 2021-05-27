8e06c93aa2187176f1d1a9b42d9f30c8b4a7d5c7
aninggo <aninggo@gmail.com>
2021-05-27 22:44:50 +0800

    update

--- recipe/recipe.go
+++ recipe/recipe.go
@@ -1,1 +1,12 @@
 package recipe
+
+type Recipe struct {
+	Version int              `json:"version"`
+	Layers  map[string]Layer `json:"layers"`
+}
+
+type Layer struct {
+	Source  string `json:"source"`
+	MinZoom uint   `json:"minzoom"`
+	MaxZoom uint   `json:"maxzoom"`
+}
